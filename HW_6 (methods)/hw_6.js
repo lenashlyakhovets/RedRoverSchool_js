3006a5d7060a1b51bd80bbe410796029572ed69b
Lena <lena.shlyakhovets@gmail.com>
2023-03-29 22:59:35 +0300

    hw6_2

--- HW_6 (methods)/hw_6.js
+++ HW_6 (methods)/hw_6.js
@@ -5,9 +5,18 @@
 // Палиндром — слово, предложение или последовательность символов, которая абсолютно одинаково читается как в привычном направлении, так и в обратном. К примеру, “Anna” — это палиндром, а “table” и “John” — нет.
 // Дана строка; нужно написать функцию, которая позволяет вернуть значение true, если строка является палиндромом, и false — если нет. При этом нужно учитывать пробелы и знаки препинания.
 
+function isPalindrome(str) {
+    // удаляем все символы, кроме букв и цифр, и приводим к нижнему регистру
+    str = str.toLowerCase().replace(/[^a-z0-9]+/g, '');
 
-
-
+    // разворачиваем строку и сравниваем с исходной
+    return str === str.split('').reverse().join('');
+}
+  
+console.log(isPalindrome('Anna')); // true
+console.log(isPalindrome('table')); // false
+console.log(isPalindrome('A man a plan a canal Panama')); // true
+console.log(isPalindrome('Was it a car or a cat I saw?')); // true
 
 
 
@@ -22,6 +31,32 @@
 [ ‘{‘, ‘(’, ‘(‘, ‘)’, ‘)’, ‘}, ‘[‘, ‘]’ ] - у всех есть пара
 [ ‘{‘, ‘(’, ‘(‘, ‘)’, ‘)’, ‘}, ‘[‘, ‘[’, ‘)’ ] - у трех нет пары*/
 
+function areBracketsPaired(str) {
+    const stack = [];
+    const brackets = {
+      "(": ")",
+      "[": "]",
+      "{": "}"
+    };
+    
+    for (let i = 0; i < str.length; i++) {
+      const char = str[i];
+      if (brackets[char]) {
+        stack.push(char);
+      } else if (char === ")" || char === "]" || char === "}") {
+        const lastBracket = stack.pop();
+        if (brackets[lastBracket] !== char) {
+          return false;
+        }
+      }
+    }
+    
+    return stack.length === 0;
+  }
+  
+  // Пример использования:
+  console.log(areBracketsPaired("{ [ ] ( ) }")); // true
+  console.log(areBracketsPaired("{ [ ( ] ) }")); // false
 
 
 
